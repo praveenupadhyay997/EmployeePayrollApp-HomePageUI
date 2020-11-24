4c4657ff076f1eae7f8b2f6a089fd7d9ecb0b06b
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-11-24 21:24:55 +0530

    [Praveen] Add: Implemented the populating of the table standing on the employee payroll home webpage.

--- js/EmployeePayrollClass.js
+++ js/EmployeePayrollClass.js
@@ -18,7 +18,7 @@ class EmployeePayRoll {
   }
   /// Defining the setter with the regex so as to match the input by the user
   set name(name) {
-    let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z]{2,}$");
+    let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z ]{2,}$");
     if (nameRegex.test(name)) this._name = name;
     else throw "Incorect Name!!!";
   }
